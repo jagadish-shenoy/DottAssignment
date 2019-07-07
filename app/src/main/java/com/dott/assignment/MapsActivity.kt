5cd9b3d4573b8767a82d04a524e9ba1e4b30b27d
Jagadish Shenoy <jagadishshenoy88@gmail.com>
2019-07-07 13:22:22 +0200

    Request the location permission once the map is working
    Handle one thing at a time to avoid complex conditions like if(isMapReady && locationPermissionHelper.isPermissionGranted)

--- app/src/main/java/com/dott/assignment/MapsActivity.kt
+++ app/src/main/java/com/dott/assignment/MapsActivity.kt
@@ -10,7 +10,6 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
@@ -31,39 +30,6 @@ class MapsActivity : AppCompatActivity(),
         (mapFragment as SupportMapFragment).getMapAsync(this)
     }
 
-    override fun onStart() {
-        super.onStart()
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
-        with(locationPermissionHelper) {
-            if (isPermissionGranted()) {
-                updateDeviceLocationOnMap()
-            } else {
-                requestPermission(this@MapsActivity)
-            }
-        }
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(_googleMap: GoogleMap) {
-        googleMap = _googleMap
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
@@ -74,13 +40,23 @@ class MapsActivity : AppCompatActivity(),
         }
     }
 
+    override fun onMapReady(_googleMap: GoogleMap) {
+        googleMap = _googleMap
+        locationPermissionHelper.apply {
+            if(isPermissionGranted()) {
+                updateDeviceLocationOnMap()
+            } else {
+                requestPermission(this@MapsActivity)
+            }
+        }
+    }
+
     private fun updateDeviceLocationOnMap() {
         /*
          * Get the best and most recent location of the device, which may be null in rare
          * cases when a location is not available.
          */
         try {
-
             val locationRequest = LocationRequest.create()
             locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
             fusedLocationProviderClient.requestLocationUpdates(
