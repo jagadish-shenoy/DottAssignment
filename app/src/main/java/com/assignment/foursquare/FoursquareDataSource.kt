99902c9fcfd533b7cee40d15d5b4468d73608797
Jagadish Shenoy <jagadishshenoy88@gmail.com>
2019-07-14 18:08:24 +0200

    Handle IOException thrown when there is no network connection!

--- app/src/main/java/com/assignment/foursquare/FoursquareDataSource.kt
+++ app/src/main/java/com/assignment/foursquare/FoursquareDataSource.kt
@@ -1,22 +1,32 @@
 package com.assignment.foursquare
 
+import java.io.IOException
+
 class FoursquareDataSource(private val venueService: VenueService) {
 
     suspend fun searchRestaurants(lat: Double, long: Double, radius:Int, limit: Int): VenueSearchResult {
+        return try {
             val response = venueService.searchVenues("$lat,$long", FOURSQUARE_CATEGORY_ID, radius, limit)
-            return if (!response.isSuccessful || response.body() == null) {
+            if (!response.isSuccessful || response.body() == null) {
                 VenueSearchResult.Failure
             } else {
                 VenueSearchResult.Success((response.body() as Venues).list)
             }
+        } catch (e:IOException) {
+            VenueSearchResult.Failure
+        }
     }
 
     suspend fun fetchRestaurantDetails(venueId: String): VenueDetailsResult {
-        val response = venueService.getVenueDetails(venueId)
-        return if (!response.isSuccessful || response.body() == null) {
+        return try {
+            val response = venueService.getVenueDetails(venueId)
+            return if (!response.isSuccessful || response.body() == null) {
+                VenueDetailsResult.Failure
+            } else {
+                VenueDetailsResult.Success(response.body() as VenueDetails)
+            }
+        } catch (e:IOException) {
             VenueDetailsResult.Failure
-        } else {
-            VenueDetailsResult.Success(response.body() as VenueDetails)
         }
     }
 }
